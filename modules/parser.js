08342a6fbe7e0a10fe601e3c5e414cc9dbf015dd
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2022-08-13 16:56:53 +0200

    fix the attribute definition

--- modules/parser.js
+++ modules/parser.js
@@ -1,30 +1,9 @@
-import { text } from './textSource.js';
+import { sourceText } from './textSource.js';
 import { listOfCreatures } from './textSource.js';
 
 // creating an array of RegExp for creatures segments
 let regArr = [];
 listOfCreatures.forEach(e => regArr.push(new RegExp(`${e}\n`)));
-
-// creating an array with creatureTemplate{} to contain txt,translated text, name, stats,etc... 
-
-let creatureTemplate = {
-    text: ``,
-    translatedText: ``,
-    name: ``,
-    description: ``,
-    attributes: {
-        strength: 0,
-        agility: 0,
-        wits: 0,
-        empathy: 0,
-    },
-    skills: {},
-    talents: {},
-    gears: {},
-    spe: ``,
-    tab: {},
-    notes: ``
-};
 
 // function to get Creature's name at i index
 let textOfRegExp = (regExp) => regExp.toString().replace(/\/|\\n/g, "");
@@ -50,10 +29,10 @@ let getCreatureText = (creatureRegExp, sourceText) => {
 
 //RegExp of attibutes 
 let attributesReg = /ATTRIBUTES:/;
-let strReg = /(?<=STRENGTH )\d+/;
-let agiReg = /(?<=AGILITY )\d+/;
-let witReg = /(?<=WITS )\d+/;
-let empReg = /(?<=EMPATHY)\d+/;
+let strReg = /(?<=STRENGTH)\s*\d+/;
+let agiReg = /(?<=AGILITY)\s*\d+/;
+let witReg = /(?<=WITS)\s*\d+/;
+let empReg = /(?<=EMPATHY)\s*\d+/;
 
 //function to get attribute 
 let getAttribute = (reg, txt) => {
@@ -63,31 +42,57 @@ let getAttribute = (reg, txt) => {
     return parseInt(resp);
 };
 
+let getDescription = (text, reg) => {
+    let descStartReg = new RegExp(`(?<=${textOfRegExp(reg)})`);
+    return text.slice(text.search(descStartReg), text.search(attributesReg)).trim()
+}
+
 
 //fill textByCreatureArray
 let creatureTable = [];
+let i = 0;
 regArr.forEach(e => {
-    let creature = Object.create(creatureTemplate);
-    creature.name = textOfRegExp(e);
-    creature.text = getCreatureText(e, text);
-    let descStartReg = new RegExp(`(?<=${textOfRegExp(e)})`);
-    creature.description = creature.text.slice(creature.text.search(descStartReg), creature.text.search(attributesReg));
-    creature.attributes.strength = getAttribute(strReg, creature.text);
-    creature.attributes.agility = getAttribute(agiReg, creature.text);
-    creature.attributes.wits = getAttribute(witReg, creature.text);
-    creature.attributes.empathy = getAttribute(empReg, creature.text);
-    creatureTable.push(creature);
-});
+    let text = getCreatureText(e, sourceText);
+    let translatedText = ``;
+    let name = textOfRegExp(e);
+    let description = ``;
+    description = getDescription(text, e);
+    let attributes = {
+        strength: 0,
+        agility: 0,
+        wits: 0,
+        empathy: 0
+    };
+    attributes.strength = getAttribute(strReg, text);
+    attributes.agility = getAttribute(agiReg, text);
+    attributes.wits = getAttribute(witReg, text);
+    attributes.empathy = getAttribute(empReg, text);
+    let skills = {};
+    let talents = {};
+    let gears = {};
+    let spe = ``;
+    let tab = {};
+    let notes = ``;
 
-console.log(creatureTable[0].description);
+    creatureTable.push({
+        text: text,
+        translatedText: translatedText,
+        name: name,
+        description: description,
+        attributes: attributes,
+        skills: skills,
+        talents: talents,
+        gears: gears,
+        spe: spe,
+        tab: tab,
+        notes: notes
 
+    })
 
-/*
-for (let i = 0; i < listOfCreatures.length; i++) {
-    if (i == listOfCreatures.length - 1) {
-        txtByCreatureArray.push(text.slice(text.search(regArr[i]),));
-        break;
-    }
-    txtByCreatureArray.push(text.slice(text.search(regArr[i]), text.search(regArr[i + 1])));
 }
-*/
+);
+console.log(creatureTable[40].name);
+console.log(creatureTable[40].attributes);
+
+
+
