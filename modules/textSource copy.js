5586fc3b675560e93fc34cec79c65238d89fedb8
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2022-08-28 00:24:32 +0200

    minor modifications

--- modules/textSource copy.js
+++ modules/textSource copy.js
@@ -1,4 +1,2 @@
 export let sourceText =
     ` COPY PDF TEXT FROM TABLE OF CONTENTS P.2 TO THE P.191 `;
-
-    console.log(FilePicker.browse('data' , 'modules/fbl-servants-of-memory-npc-parser/assets'))
