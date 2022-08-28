bc41db7b2a6515932547a1f4d7834023cc53484d
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2022-08-28 02:08:13 +0200

    remove unused files Finalize parsing by input text

--- modules/character-generator.js
+++ modules/character-generator.js
@@ -1,4 +1,5 @@
-import { creatureTable } from './parser.js'
+import { parseCreatures } from './parser.js'
+
 const moduleData = {
     moduleFlag: "servants-of-memory-parser",
     moduleKey: "fbl-servants-of-memory-npc-parser",
@@ -36,9 +37,37 @@ class ImportFormWrapper extends FormApplication {
         return ui.notifications.warn("This version of the core module is not compatible with 0.7.9");
     }
 }
-
 let welcome = async (data) => {
+    let inputDialog = async()=>{
+        let hbs = await renderTemplate(`modules/${data.moduleKey}/templates/text-input.hbs`,{});
+        const dialogInput = new Dialog({
+            title:'Copy text form Servants of memory',
+            content : hbs,
+            buttons: {
+                go: {
+                  icon: '<i class="fas fa-check"></i>',
+                  label: 'Go',
+                  callback: async html =>{
+                    let text = await html.find('textarea#text-input').val();
+    let creatureArr = await parseCreatures(text);
+setTimeout(()=>{
+    console.log("generateActor"),
+    generateCreatures(creatureArr)
+},8000
 
+)
+
+                }
+                },
+                cancel: {
+                  icon: '<i class="fas fa-times"></i>',
+                  label: 'Cancel',
+                  callback: () => console.log('Chose cancel'),
+                },
+              }
+        })
+        dialogInput.render(true);
+    }
     //game.settings.set(data.moduleKey, "initialized", true);
     const dialog = new Dialog({
         title: `Import ${data.moduleTitle}`,
@@ -47,10 +76,8 @@ let welcome = async (data) => {
             initialize: {
                 label: "Begin!",
                 callback: async () => {
-
                     await createFolder();
-                    generateCreatures();
-
+                    inputDialog();
                 }
             },
 
@@ -74,94 +101,97 @@ let createFolder = async () => {
 }
 
 let skillsNameArr = ["might", "endure", "melee", "crafting", "stealth", "sleight-of-hand", "move", "marksmanship", "scout", "scouting", "lore", "survive", "insight", "manipulate", "performance", "healing", "animal"]
-let generateCreatures = () => {
+let generateCreatures = (creatureTable) => {
     creatureTable.forEach(async creature => {
-        let actor = await Actor.create({
-            name: creature.name,
-            type: "monster",
-            folder: game.folders.getName(moduleData.moduleFolderNameDict['Servants of Memory actors']),
-            img: creature.img,
-            data: {
-                attribute: {
-                    strength: {
-                        value: creature.attributes.strength,
-                        max: creature.attributes.strength
-                    },
-                    agility: {
-                        value: creature.attributes.agility,
-                        max: creature.attributes.agility
-                    },
-                    wits: {
-                        value: creature.attributes.wits,
-                        max: creature.attributes.wits
-                    },
-                    empathy: {
-                        value: creature.attributes.empathy,
-                        max: creature.attributes.empathy
+
+                    let actor = await Actor.create({
+                        name: creature.name,
+                        type: "monster",
+                        folder: game.folders.getName(moduleData.moduleFolderNameDict['Servants of Memory actors']),
+                        img: creature.img,
+                        data: {
+                            attribute: {
+                                strength: {
+                                    value: creature.attributes.strength,
+                                    max: creature.attributes.strength
+                                },
+                                agility: {
+                                    value: creature.attributes.agility,
+                                    max: creature.attributes.agility
+                                },
+                                wits: {
+                                    value: creature.attributes.wits,
+                                    max: creature.attributes.wits
+                                },
+                                empathy: {
+                                    value: creature.attributes.empathy,
+                                    max: creature.attributes.empathy
+                                }
+                            },
+                            movement: { value: creature.movement },
+                            armor: {
+                                value: creature.armor
+                            },
+                            bio: {
+                                note: {
+                                    value: creature.text
+                                }
+                            }
+                        },
+                        token: {
+                            name: creature.name,
+                            img: creature.imgToken
+                        }
+                    });
+                    //update skills if specified
+                    if (creature.skills) {
+            
+                        skillsNameArr.forEach(i => {
+                            if (creature.skills.hasOwnProperty(i)) {
+                                let skillToFill = `data.skill.${i}.value`;
+                                //handeling  exceptions
+                                if (i == "animal") {
+                                    skillToFill = `data.skill.animal-handling.value`;
+                                }
+                                if (i == "scout") {
+                                    skillToFill = 'data.skill.scouting.value';
+                                }
+                                if (i == "manipulate") {
+                                    skillToFill = 'data.skill.manipulation.value';
+                                }
+                                if (i == "survive") {
+                                    skillToFill = 'data.skill.survival.value';
+                                }
+                                if (i == "endure") {
+                                    skillToFill = 'data.skill.endurance.value';
+                                }
+            
+                                actor.update({ [skillToFill]: creature.skills[i] });
+                            }
+                        })
                     }
-                },
-                movement: { value: creature.movement },
-                armor: {
-                    value: creature.armor
-                },
-                bio: {
-                    note: {
-                        value: creature.text
+            
+                    //update gears
+            
+                        if(creature.gears){
+                            creature.gears.forEach( async e=>
+                               actor.createEmbeddedDocuments("Item",[e])
+                            )
+                        }
+                    //update attacks
+            
+                    if(creature.attacks){
+                        creature.attacks.forEach( async e=>
+                           actor.createEmbeddedDocuments("Item",[e])
+                        )
                     }
+            
+            
                 }
-            },
-            token: {
-                name: creature.name,
-                img: creature.imgToken
-            }
-        });
-        //update skills if specified
-        if (creature.skills) {
-
-            skillsNameArr.forEach(i => {
-                if (creature.skills.hasOwnProperty(i)) {
-                    let skillToFill = `data.skill.${i}.value`;
-                    //handeling  exceptions
-                    if (i == "animal") {
-                        skillToFill = `data.skill.animal-handling.value`;
-                    }
-                    if (i == "scout") {
-                        skillToFill = 'data.skill.scouting.value';
-                    }
-                    if (i == "manipulate") {
-                        skillToFill = 'data.skill.manipulation.value';
-                    }
-                    if (i == "survive") {
-                        skillToFill = 'data.skill.survival.value';
-                    }
-                    if (i == "endure") {
-                        skillToFill = 'data.skill.endurance.value';
-                    }
-
-                    actor.update({ [skillToFill]: creature.skills[i] });
-                }
-            })
-        }
-
-        //update gears
-
-            if(creature.gears){
-                creature.gears.forEach( async e=>
-                   actor.createEmbeddedDocuments("Item",[e])
                 )
             }
-        //update attacks
 
-        if(creature.attacks){
-            creature.attacks.forEach( async e=>
-               actor.createEmbeddedDocuments("Item",[e])
-            )
-        }
-
-
-    }
-    )
-}
+        
 Hooks.on('renderSidebarTab', (app, html) => {
     if (app.options.id == 'actors') {
         createActorButton();
